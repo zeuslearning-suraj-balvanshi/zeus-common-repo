1ea87305e683ce52d005a5ce47ac6d8016520be3
zeuslearning-suraj-balvanshi <suraj.balvanshi@zeuslearning.com>
2021-07-09 20:36:19 +0530

    Edited all three js

--- 1.js
+++ 1.js
@@ -2,3 +2,11 @@ console.log("This is file 1")
 console.log("git trial");
 console.log("This is file 1")
 console.log("changing 3 files")
+
+function() {
+    console.log("Suraj")
+    console.log("Suraj");
+    console.log("Suraj")
+    console.log("Suraj")
+
+}
--- 2.js
+++ 2.js
@@ -1,2 +1,11 @@
 console.log("This is file 2")
 console.log("changing 3 files")
+
+
+function() {
+    console.log("Suraj")
+    console.log("Suraj");
+    console.log("This is file 1")
+    console.log("changing 3 files")
+
+}
